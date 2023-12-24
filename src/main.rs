557419c013d9ddc862a28334e9a32a9e9697113c
banana <castiglione@mac.com>
2023-12-24 10:07:17 +0800

    identify and rotate the player orb

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
 use bevy_ecs_ldtk::prelude::*;
+use std::f32::consts::PI;
 
 #[derive(Bundle, LdtkEntity)]
 struct PlayerBundle {
+    instance: EntityInstance,
     #[sprite_sheet_bundle]
     sprite_bundle: SpriteSheetBundle,
 }
@@ -22,6 +24,7 @@ fn main() {
             LdtkPlugin,
         ))
         .add_systems(Startup, startup)
+        .add_systems(Update, move_player)
         .insert_resource(LevelSelection::Index(0))
         .register_ldtk_entity::<PlayerBundle>("Player")
         .run();
@@ -46,4 +49,11 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ldtk_handle: asset_server.load("levels.ldtk"),
         ..default()
     });
+}
+
+fn move_player(mut entities: Query<(&EntityInstance, &mut Transform)>, time: Res<Time>) {
+    let arc = PI * -2.0 * time.delta_seconds();
+    for (_, mut transform) in entities.iter_mut() {
+        transform.rotate(Quat::from_rotation_z(arc));
+    }
 }
